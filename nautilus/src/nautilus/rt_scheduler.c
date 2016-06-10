1866c9bdfc92c034dfba97542caf6bc968b5532a
ChrisBeauchene <cbeauchene93@gmail.com>
2016-06-10 05:25:07 -0500

    Pushing to test

--- nautilus/src/nautilus/rt_scheduler.c
+++ nautilus/src/nautilus/rt_scheduler.c
@@ -1468,7 +1468,7 @@ static rt_thread_sim* rt_need_resched_logic(rt_simulator *simulator, rt_thread_s
         if (simulator->pending->threads[0]->deadline < time)
         {
             rt_thread_sim *arrived_thread = dequeue_thread_logic(simulator->pending);
-            update_periodic(thread, time);
+            update_periodic_logic(thread, time);
             enqueue_thread_logic(simulator->runnable, arrived_thread);
             continue;
         } else
