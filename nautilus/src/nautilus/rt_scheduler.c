210927ddf916d5a05ab0fc7dfeae22378dd885e2
ChrisBeauchene <cbeauchene93@gmail.com>
2016-06-10 02:53:16 -0500

    Pushing to test

--- nautilus/src/nautilus/rt_scheduler.c
+++ nautilus/src/nautilus/rt_scheduler.c
@@ -224,6 +224,7 @@ rt_thread* list_remove(rt_list *l, rt_thread *t) {
             }
             n->next = NULL;
             n->prev = NULL;
+            n->status = REMOVED;
             return n->thread;
         }
     }
